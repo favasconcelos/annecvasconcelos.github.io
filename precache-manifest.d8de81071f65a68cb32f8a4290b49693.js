f0926eb625dd0841e052ff1cdcbbecde105cd153
Felipe Azevedo <felipe.azevedo@adidas.com>
2019-08-05 12:16:08 +0200

    Updates

---
+++ precache-manifest.d8de81071f65a68cb32f8a4290b49693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5592b40beb7eb714102cda1b7946ef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd6eb4f57a0e4aa09118",
+    "url": "/static/css/main.23adba1b.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dd6eb4f57a0e4aa09118",
+    "url": "/static/js/main.d84fdf6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7eb2d3edfab0ce3e97505b2e324f46cd",
+    "url": "/static/media/gallery.7eb2d3ed.jpg"
+  }
+]);
