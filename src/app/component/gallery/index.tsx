e4847aefc5d2e853bd366197eb23ce9b8876e4fb
Felipe Vasconcelos <f@avasconcelos.com>
2019-08-14 00:38:39 +0200

    Updating gallery animation

--- src/app/component/gallery/index.tsx
+++ src/app/component/gallery/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 // local
 import './gallery.scss';
@@ -7,36 +7,36 @@ import { IMAGES, filterByOption } from 'app/services/gallery';
 
 const GalleryComponent: React.FC = () => {
   const [option, setOption] = useState(GalleryOptions.ALL);
-  const onClick = useCallback((value) => setOption(value), []);
   const images = filterByOption(IMAGES, option);
 
+  console.log(images);
   return (
     <div id="gallery">
       <ul className="options">
         {Object.values(GalleryOptions).map(value => {
           const key = `gallery-option-${value}`;
           const className = value === option ? 'active' : '';
-          return <li key={key} className={className} onClick={() => onClick(value)}>{value}</li>;
+          return (
+            <li key={key} className={className} onClick={() => setOption(value)}>
+              {value}
+            </li>
+          );
         })}
       </ul>
-      <TransitionGroup
-        component="div"
-        className="images">
+      <TransitionGroup component="div" className="images">
         {images.map(image => {
           const key = `gallery-item-${image.id}`;
-          return <CSSTransition
-            key={key}
-            timeout={500}
-            classNames="item"
-          >
-            <div key={key} className="item">
-              <img src={image.src} alt={image.title} />
-              <div className="meta">
-                <h3>{image.title}</h3>
-                <p>{image.description}</p>
+          return (
+            <CSSTransition key={key} timeout={500} classNames="fade">
+              <div className="item">
+                <img src={image.src} alt={image.title} />
+                <div className="meta">
+                  <h3>{image.title}</h3>
+                  <p>{image.description}</p>
+                </div>
               </div>
-            </div>
-          </CSSTransition>
+            </CSSTransition>
+          );
         })}
       </TransitionGroup>
     </div>
